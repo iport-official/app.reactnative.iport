cd3f83f23ce4426d9f555c78c89348d62d1e987c
caique28 <caiquetorres.bastos2807@gmail.com>
2020-08-21 21:23:17 -0300

    feat(poppins): add new font

--- App.tsx
+++ App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { AppLoading } from "expo";
 
 import AppStack from './src/routes/AppStack'
 
+import { Poppins_400Regular, Poppins_600SemiBold, useFonts } from '@expo-google-fonts/poppins'
+
+let fontsLoaded: boolean;
+
+function LoadFonts() {
+    [fontsLoaded] = useFonts({
+        Poppins_400Regular,
+        Poppins_600SemiBold
+    })
+}
+
 export default function App() {
+
+    LoadFonts()
+
+    if (!fontsLoaded) {
+        return <AppLoading />
+    }
+
     return (
         <AppStack />
     );
