d41646bf0c563384278268ffc1abea5952c874ab
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-08-23 17:12:46 -0300

    feat(Drawer): add icons in each page option

--- src/components/DrawerContent/index.tsx
+++ src/components/DrawerContent/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import { Text } from 'react-native'
 import {
     DrawerContentComponentProps,
-    DrawerContentOptions
+    DrawerContentOptions,
+    DrawerItem
 } from '@react-navigation/drawer'
+
+import { MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+
+import ProfilePhoto from '../ProfilePhoto'
 
 import {
     ContainerView,
     ContentDrawerContentScrollView,
+    ProfileView,
+    ProfileText,
+    ContentView,
     FooterView
 } from './styles'
+
+import photo from '../../assets/foto_example.png'
 
 const DrawerContent: React.FC<DrawerContentComponentProps<DrawerContentOptions>> = (props) => {
     return (
@@ -17,14 +27,72 @@ const DrawerContent: React.FC<DrawerContentComponentProps<DrawerContentOptions>>
 
         <ContainerView>
             <ContentDrawerContentScrollView {...props} >
-                <Text>
-                    Content
-                </Text>
+                <ProfileView>
+                    <ProfilePhoto
+                        size={75}
+                        source={photo}
+                    />
+                    <ProfileText>Scarlett Johansson</ProfileText>
+                </ProfileView>
+                <ContentView>
+                    <DrawerItem
+                        icon={({ color, size }) =>
+                            <MaterialIcons
+                                name="home"
+                                size={size}
+                                color={color}
+                            />
+                        }
+                        label="Início"
+                        onPress={() => { }}
+                    />
+                    <DrawerItem
+                        icon={({ color, size }) =>
+                            <MaterialIcons
+                                name="person"
+                                size={size}
+                                color={color}
+                            />
+                        }
+                        label="Perfil"
+                        onPress={() => { }}
+                    />
+                    <DrawerItem
+                        icon={({ color, size }) =>
+                            <MaterialIcons
+                                name="settings"
+                                size={size}
+                                color={color}
+                            />
+                        }
+                        label="Configurações"
+                        onPress={() => { }}
+                    />
+                    <DrawerItem
+                        icon={({ color, size }) =>
+                            <MaterialIcons
+                                name="help"
+                                size={size}
+                                color={color}
+                            />
+                        }
+                        label="Ajuda"
+                        onPress={() => { }}
+                    />
+                </ContentView>
             </ContentDrawerContentScrollView>
             <FooterView>
-                <Text>
-                    Footer
-                </Text>
+                <DrawerItem
+                    icon={({ color, size }) =>
+                        <FontAwesome
+                            name="sign-out"
+                            size={size}
+                            color={color}
+                        />
+                    }
+                    label="Sair"
+                    onPress={() => { }}
+                />
             </FooterView>
         </ContainerView>
 
--- src/components/DrawerContent/styles.ts
+++ src/components/DrawerContent/styles.ts
@@ -10,6 +10,21 @@ export const ContentDrawerContentScrollView = styled(DrawerContentScrollView)`
     
 `
 
+export const ProfileView = styled.View`
+    align-items: center;
+    flex-direction: row;
+    padding-bottom: 20px;
+    padding-left: 15px;
+`
+
+export const ProfileText = styled.Text`
+    margin-left: 15px;
+`
+
+export const ContentView = styled.View`
+    
+`
+
 export const FooterView = styled.View`
 
 `
