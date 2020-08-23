e426005dabd465a3403784c9dc7a344cf7f7a055
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-08-23 18:30:04 -0300

    feat(DrawerItem): create the main files of the Drawer item

---
+++ src/components/DrawerContent/DrawerItem/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { ContainerView } from './styles'
+
+export default function DrawerItem() {
+    //#region JSX
+
+    <ContainerView>
+
+    </ContainerView>
+
+    //#endregion
+}
