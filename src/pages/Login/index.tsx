41c93dea8f8e33c1ad9f9a37169982cad5148ea2
Michell Algarra <michell.algarra@gmail.com>
2020-08-23 21:47:24 -0300

    feat(login-page): Created a Login Screen

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import { View } from 'react-native'
-import { StackScreenProps } from '@react-navigation/stack'
+import React from 'react';
+import { View, Text } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
+import { setStatusBarHidden, setStatusBarStyle } from 'expo-status-bar';
 
-import { AppStackParamsList } from '../../routes/AppStack'
+import { AppStackParamsList } from '../../routes/AppStack';
+import TextField from '../../components/TextField';
+import Checkbox from '../../components/Checkbox';
+import FormButton from '../../components/FormButton';
+
+import { LoginContainer, LoginLogo, LoginFooter,
+    CheckboxContainer, CheckboxText, ForgotPassword } from './styles';
+import { colors } from '../../styles';
 
 type DefaultLoginPageProps = StackScreenProps<
     AppStackParamsList,
@@ -10,7 +18,29 @@ type DefaultLoginPageProps = StackScreenProps<
 >
 
 export default function LoginPage({ navigation }: DefaultLoginPageProps) {
+
+    setStatusBarHidden(true, "slide");
+    setStatusBarStyle('dark');
+
     return (
-        <View />
+        <LoginContainer>
+            <LoginLogo source={require('../../assets/icon.png')}></LoginLogo>
+
+            <TextField label='E-mail'/>
+            <TextField label='Senha'/>
+
+            <LoginFooter>
+                <CheckboxContainer>
+                    <Checkbox />
+                    <CheckboxText>Lembrar</CheckboxText>
+                </CheckboxContainer>
+                <ForgotPassword onPress={() => alert('Esqueceu?')}>Esqueceu a senha?</ForgotPassword>
+            </LoginFooter>
+
+            <FormButton label='Login' color={colors.grayPurple}
+                disableColor={colors.grayPurple + '88'}
+                ripple={colors.lightPurple}/>
+
+        </LoginContainer>
     )
 }
