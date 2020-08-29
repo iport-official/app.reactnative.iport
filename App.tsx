905f5ef7d42c084f9bc0b55807753f1fc5a903e6
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-08-28 22:48:58 -0300

    feat(App.tsx): load font in the start of the app

--- App.tsx
+++ App.tsx
@@ -5,13 +5,16 @@ import { enableScreens } from 'react-native-screens';
 import AppStack from './src/routes/AppStack'
 
 import { Poppins_400Regular, Poppins_600SemiBold, useFonts } from '@expo-google-fonts/poppins'
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 let fontsLoaded: boolean;
 
 function LoadFonts() {
     [fontsLoaded] = useFonts({
         Poppins_400Regular,
-        Poppins_600SemiBold
+        Poppins_600SemiBold,
+        Roboto_400Regular,
+        Roboto_700Bold
     })
 }
 
