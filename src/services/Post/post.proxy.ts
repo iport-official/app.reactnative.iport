11d0d4ada6c7c5f8ce3714b29b9df7fbe044fe12
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-06 16:14:15 -0300

    feat(post.proxy.ts): create the file post.proxy.ts

---
+++ src/services/Post/post.proxy.ts
@@ -0,0 +1,15 @@
+export interface PostProxy {
+    id: string
+    image: string
+    title: string
+    description: string
+    category: string
+    recomendation: number
+    contact: string
+    salary: number
+    post: string
+    local: string
+    requirements: string
+    experienceLevel: string
+    vacancyDescription: string
+}
