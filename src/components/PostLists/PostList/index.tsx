50ac3690c33a1a73ae2e561b4aa539cae3bdfc16
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-06 21:12:43 -0300

    feat(PostList): removed unused data and styles, modularized the funtions of that component

--- src/components/PostLists/PostList/index.tsx
+++ src/components/PostLists/PostList/index.tsx
@@ -1,41 +1,20 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useRef } from 'react'
 import { ViewToken } from 'react-native'
-import * as SecureStore from 'expo-secure-store';
 
 import PostItem from '../../PostItem'
 
 import {
     ContainerView,
-    TitleText,
     PostFlatList
 } from './styles'
 
 import { PostProxy } from '../../../services/Post/post.proxy'
 
-import api from '../../../services/api'
-
 export interface PostListProps {
-    category: string
+    data: PostProxy[]
 }
 
-const PostList: React.FC<PostListProps> = ({ category }) => {
-
-    const [data, setData] = useState<PostProxy[]>([])
-
-    async function loadData(category: string) {
-        const token = await SecureStore.getItemAsync('access_token')
-        const response = await api.get<PostProxy[]>(`posts/recomendations?category=${category}&page=0`, {
-            headers: {
-                Authorization: 'Bearer ' + token
-            }
-        })
-
-        setData(response.data)
-    }
-
-    useEffect(() => {
-        loadData(category)
-    }, [])
+const PostList: React.FC<PostListProps> = ({ data }) => {
 
     const handleViewabilityConfig = useRef({ viewAreaCoveragePercentThreshold: 50 })
 
@@ -50,15 +29,16 @@ const PostList: React.FC<PostListProps> = ({ category }) => {
         //#region JSX
 
         <ContainerView>
-            <TitleText>{category}</TitleText>
             <PostFlatList
                 horizontal
                 showsHorizontalScrollIndicator={false}
                 data={data}
                 renderItem={({ item }) => {
-                    const { ...rest } = item
+                    const { id, ...rest } = item
                     return (
                         <PostItem
+                            key={id}
+                            id={id}
                             {...rest}
                         />
                     )
--- src/components/PostLists/PostList/styles.ts
+++ src/components/PostLists/PostList/styles.ts
@@ -1,23 +1,11 @@
-import { Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 import styled from 'styled-components/native';
 import { FlatList } from 'react-native';
 
 import { PostItemProps } from '../../PostItem';
-import { colors } from '../../../styles';
 
 export const ContainerView = styled.View`
     flex: 1;
     margin-top: 10px;
-`
-
-export const TitleText = styled.Text`
-    font-family: Poppins_600SemiBold;
-    text-shadow: 0px 0px 1px ${colors.lighterVividPurple}80;
-    font-size: 21px;
-
-    color: ${colors.livePurple};
-    margin: 7px 0 10px 0;
-    padding-left: 30px;
 `
 
 export const PostFlatList = styled(FlatList as new () => FlatList<PostItemProps>)`
