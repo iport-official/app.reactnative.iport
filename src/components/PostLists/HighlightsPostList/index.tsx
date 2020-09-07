52a8b0b3de951cb803c9f2e6db96bed522756eb3
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-06 22:41:14 -0300

    feat(HighlightsPostList): moved all content from PostList to HighlightsPostList

--- src/components/PostLists/HighlightsPostList/index.tsx
+++ src/components/PostLists/HighlightsPostList/index.tsx
@@ -1,26 +1,44 @@
-import React, { useState, useEffect } from 'react'
+import React, {
+    useState,
+    useEffect,
+    useRef
+} from 'react'
+import { ViewToken } from 'react-native';
 import { getItemAsync } from 'expo-secure-store';
 
-import PostList from '../PostList';
+import PostItem from '../../PostItem';
 
 import {
     ContainerView,
-    TitleText
+    TitleText,
+    EndFlatListActivityIndicator,
+    PostFlatList
 } from './styles'
 
-import api from '../../../services/api';
 import { PostProxy } from '../../../services/Post/post.proxy';
+
+import api from '../../../services/api';
 
 interface HighlightsPostListProps {
     title: string
 }
 
 const HighlightsPostList: React.FC<HighlightsPostListProps> = ({ title }) => {
-
-    const [page, setPage] = useState(0)
+    const [page, setPage] = useState<number>(0)
+    const [loading, setLoading] = useState<boolean>(false)
     const [data, setData] = useState<PostProxy[]>([])
 
+    const handleViewabilityConfig = useRef({ viewAreaCoveragePercentThreshold: 50 })
+
+    const handleOnViewableItemsChanged = useRef((viewableItems: {
+        viewableItems: ViewToken[];
+        changed: ViewToken[];
+    }) => {
+        // console.log(viewableItems)
+    })
+
     async function loadData(pageNumber: number = page) {
+        setLoading(true)
         const token = await getItemAsync('access_token')
         const response = await api.get<PostProxy[]>(`posts/highlights?page=${pageNumber}`, {
             headers: {
@@ -32,6 +50,7 @@ const HighlightsPostList: React.FC<HighlightsPostListProps> = ({ title }) => {
             ...response.data
         ])
         setPage(pageNumber + 1)
+        setLoading(false)
     }
 
     useEffect(() => { loadData() }, [])
@@ -41,10 +60,35 @@ const HighlightsPostList: React.FC<HighlightsPostListProps> = ({ title }) => {
 
         <ContainerView>
             <TitleText>{title}</TitleText>
-            <PostList
+            <PostFlatList
+                horizontal
+                showsHorizontalScrollIndicator={false}
                 data={data}
-                onEndReached={loadData}
+                onEndReached={() => { loadData() }}
                 onEndReachedThreshold={0.1}
+                ListFooterComponent={(
+                    <EndFlatListActivityIndicator
+                        style={{
+                            display: loading ? 'flex' : 'none'
+                        }}
+                    />
+                )}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => {
+                    const { id, ...rest } = item
+                    return (
+                        <PostItem
+                            key={id}
+                            id={id}
+                            {...rest}
+                        />
+                    )
+                }}
+                onViewableItemsChanged={handleOnViewableItemsChanged.current}
+                viewabilityConfig={handleViewabilityConfig.current}
+                contentContainerStyle={{
+                    alignItems: "center"
+                }}
             />
         </ContainerView>
 
