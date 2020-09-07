70c96cf36cd932fedee227d11620856fdbff31ae
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-06 22:41:59 -0300

    feat(PostList): removed the code of those files, they will be changed in the future

--- src/components/PostLists/PostList/index.tsx
+++ src/components/PostLists/PostList/index.tsx
@@ -1,61 +1,9 @@
-import React, { useRef } from 'react'
-import { ViewToken } from 'react-native'
+import React from 'react'
+import { View } from 'react-native'
 
-import PostItem from '../../PostItem'
-
-import {
-    ContainerView,
-    PostFlatList
-} from './styles'
-
-import { PostProxy } from '../../../services/Post/post.proxy'
-
-export interface PostListProps {
-    onEndReached?(): void
-    onEndReachedThreshold?: number
-    data: PostProxy[]
-}
-
-const PostList: React.FC<PostListProps> = ({ onEndReached, onEndReachedThreshold, data }) => {
-    const handleViewabilityConfig = useRef({ viewAreaCoveragePercentThreshold: 50 })
-
-    const handleOnViewableItemsChanged = useRef((viewableItems: {
-        viewableItems: ViewToken[];
-        changed: ViewToken[];
-    }) => {
-        // console.log(viewableItems)
-    })
-
+const PostList = () => {
     return (
-        //#region JSX
-
-        <ContainerView>
-            <PostFlatList
-                horizontal
-                onEndReached={() => {
-                    if (onEndReached)
-                        onEndReached()
-                }}
-                onEndReachedThreshold={onEndReachedThreshold}
-                showsHorizontalScrollIndicator={false}
-                data={data}
-                renderItem={({ item }) => {
-                    const { id, ...rest } = item
-                    return (
-                        <PostItem
-                            key={id}
-                            id={id}
-                            {...rest}
-                        />
-                    )
-                }}
-                keyExtractor={item => item.id}
-                onViewableItemsChanged={handleOnViewableItemsChanged.current}
-                viewabilityConfig={handleViewabilityConfig.current}
-            />
-        </ContainerView>
-
-        //#endregion
+        <View />
     )
 }
 
