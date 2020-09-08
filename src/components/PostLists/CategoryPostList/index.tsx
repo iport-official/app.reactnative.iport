1e17f691137019e974ad984b7ee8d2773fc69a34
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-08 20:39:38 -0300

    test(CategoryPostList): testing the new redux implementation in categories

--- src/components/PostLists/CategoryPostList/index.tsx
+++ src/components/PostLists/CategoryPostList/index.tsx
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import { getItemAsync } from 'expo-secure-store'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { ApplicationState } from '../../../store'
+import { CategoriesTypes } from '../../../store/ducks/categories/types'
+
+import CategoryList from '../../CategoryList'
 
 import {
     ContainerView,
     TitleText
 } from './styles'
-import CategoryList from '../../CategoryList'
 
-import { CategoryProxy } from '../../../services/Category/category.proxy'
-
-import api from '../../../services/api'
 
 interface CategoryPostListProps {
     title: string
@@ -17,44 +18,58 @@ interface CategoryPostListProps {
 
 const CategoryPostList: React.FC<CategoryPostListProps> = ({ title }) => {
 
-    const [categoryPage, setCategoryPage] = useState<number>(0)
-    const [loadingCategories, setLoadingCategories] = useState<boolean>(false)
-    const [categoryData, setCategoryData] = useState<CategoryProxy[]>([])
+    const categories = useSelector((state: ApplicationState) => state.categories.data.array)
+    const dispatch = useDispatch()
 
-    async function loadCategoryData(pageNumber: number = categoryPage) {
-
-        setLoadingCategories(true)
-
-        const token = await getItemAsync('access_token')
-        const response = await api.get<CategoryProxy[]>(`categories?page=${categoryPage}`, {
-            headers: {
-                Authorization: 'Bearer ' + token
-            }
-        })
-
-        setCategoryData([
-            ...categoryData,
-            ...response.data
-        ])
-        setCategoryPage(pageNumber + 1)
-
-        setLoadingCategories(false)
-
+    function loadRequest() {
+        dispatch({ type: CategoriesTypes.LOAD_REQUEST })
     }
 
-    useEffect(() => { loadCategoryData() }, [])
+    // const [categoryPage, setCategoryPage] = useState<number>(0)
+    // const [loadingCategories, setLoadingCategories] = useState<boolean>(false)
+    // const [categoryData, setCategoryData] = useState<CategoryProxy[]>([])
+
+    // async function loadCategoryData(pageNumber: number = categoryPage) {
+
+    //     setLoadingCategories(true)
+
+    //     const token = await getItemAsync('access_token')
+    //     const response = await api.get<CategoryProxy[]>(`categories?page=${categoryPage}`, {
+    //         headers: {
+    //             Authorization: 'Bearer ' + token
+    //         }
+    //     })
+
+    //     setCategoryData([
+    //         ...categoryData,
+    //         ...response.data
+    //     ])
+    //     setCategoryPage(pageNumber + 1)
+
+    //     setLoadingCategories(false)
+
+    // }
+
+    useEffect(() => { loadRequest() }, [])
 
     return (
         //#region JSX
 
         <ContainerView>
             <TitleText>{title}</TitleText>
-            <CategoryList categories={categoryData} />
+            <CategoryList categories={categories} />
         </ContainerView>
 
         //#endregion
     )
 
 }
+
+// const mapStateToProps = (state: ApplicationState) => ({
+//     categories: state.categories.data
+// })
+
+// const mapDispatchToProps = (dispatch: Dispatch) =>
+//     bindActionCreators(CategoriesActions, dispatch)
 
 export default CategoryPostList
