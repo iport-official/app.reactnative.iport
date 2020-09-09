b0268654efc7f713a385bb3ebbc819bb64f62ed4
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-08 22:06:24 -0300

    style(categories/types.ts): remove some comments

--- src/store/ducks/categories/types.ts
+++ src/store/ducks/categories/types.ts
@@ -1,8 +1,5 @@
 import { BaseArrayProxy } from "../../../services/base-array-proxy";
 
-/**
- * Action types
- */
 export enum CategoriesTypes {
     LOAD_REQUEST = '@categories/LOAD_REQUEST',
     LOAD_SUCCESS = '@categories/LOAD_SUCCESS',
@@ -10,18 +7,12 @@ export enum CategoriesTypes {
     SELECT = '@categories/SELECT'
 }
 
-/**
- * Data types
- */
 export interface CategoryProxy {
     id: string
     name: string
     category: string
 }
 
-/**
- * State type
- */
 export interface CategoriesState {
     readonly data: BaseArrayProxy<CategoryProxy>
     readonly loading: boolean
