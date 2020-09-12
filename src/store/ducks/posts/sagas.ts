49d24320a0bebfe979418741886aef66ba5d7f6c
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-12 14:48:35 -0300

    feat(posts/sagas.ts): better api calling and add the shouldStart property in payload, that allow the list knows how to render the items in screen

--- src/store/ducks/posts/sagas.ts
+++ src/store/ducks/posts/sagas.ts
@@ -1,6 +1,6 @@
 import { AxiosResponse } from "axios";
 import { getItemAsync } from "expo-secure-store";
-import { call, put } from "redux-saga/effects";
+import { call, put, select } from "redux-saga/effects";
 
 import { BaseArrayProxy } from "../../../services/base-array-proxy";
 
@@ -14,10 +14,12 @@ import {
 } from "./actions";
 
 import { PostProxy, PostsTypes } from "./types";
+import { ApplicationState } from "../..";
 
 interface LoadPostsHighlightsActions {
     type: typeof PostsTypes.LOAD_POSTS_HIGHLIGHTS_REQUEST
     payload: {
+        shouldStart: boolean
         pageNumber: number
     }
 }
@@ -25,22 +27,37 @@ interface LoadPostsHighlightsActions {
 interface LoadPostsByCategoryAction {
     type: typeof PostsTypes.LOAD_POSTS_BY_CATEGORY_REQUEST
     payload: {
+        shouldStart: boolean
         category: string
         pageNumber: number
     }
 }
+
+const getPostsState = (state: ApplicationState) => state.posts
 
 export function* loadPostsHighlights({ payload }: LoadPostsHighlightsActions) {
     try {
         const token = yield Promise.resolve(getItemAsync('access_token'))
         const response: AxiosResponse<BaseArrayProxy<PostProxy>> = yield call(
             api.get,
-            `highlights?page=${payload.pageNumber}`, {
+            `posts/highlights?page=${payload.pageNumber}`, {
             headers: {
                 Authorization: 'Bearer ' + token
             }
         })
-        yield put(loadPostsHighlightsSuccess(response.data))
+
+        const { highlights }: ReturnType<typeof getPostsState> = yield select(getPostsState)
+        const data = payload.shouldStart
+            ? response.data
+            : {
+                lenght: highlights.lenght + response.data.lenght,
+                array: [
+                    ...highlights.array,
+                    ...response.data.array
+                ]
+            }
+
+        yield put(loadPostsHighlightsSuccess(data))
     } catch (error) {
         yield put(loadPostsHighlightsFailure())
     }
@@ -51,12 +68,24 @@ export function* loadPostsByCategory({ payload }: LoadPostsByCategoryAction) {
         const token = yield Promise.resolve(getItemAsync('access_token'))
         const response: AxiosResponse<BaseArrayProxy<PostProxy>> = yield call(
             api.get,
-            `categories?category=${payload.category}&page=${payload.pageNumber}`, {
+            `posts/categories?category=${payload.category}&page=${payload.pageNumber}`, {
             headers: {
                 Authorization: 'Bearer' + token
             }
         })
-        yield put(loadPostsByCategorySuccess(response.data))
+
+        const { categories }: ReturnType<typeof getPostsState> = yield select(getPostsState)
+        const data = payload.shouldStart
+            ? response.data
+            : {
+                lenght: categories.lenght + response.data.lenght,
+                array: [
+                    ...categories.array,
+                    ...response.data.array
+                ]
+            }
+
+        yield put(loadPostsByCategorySuccess(data))
     } catch (error) {
         yield put(loadPostsByCategoryFailure())
     }
