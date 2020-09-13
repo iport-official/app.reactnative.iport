5a09b37fa169cfc6995f0101abf26d37586ee0ef
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-13 15:49:34 -0300

    refactor(posts): split the posts in highlightsPosts and categoriesPosts (duck pattern)

---
+++ src/store/ducks/categoriesPosts/index.ts
@@ -0,0 +1,39 @@
+import { Reducer } from "redux";
+import { CategoriesPostsState, CategoriesPostsTypes } from "./types";
+
+const INITIAL_STATE: CategoriesPostsState = {
+    categoriesPosts: {
+        length: 0,
+        array: []
+    },
+    loadingCategoriesPosts: false,
+    errorCategoriesPosts: false
+}
+
+const reducer: Reducer<CategoriesPostsState> = (state = INITIAL_STATE, action) => {
+
+    switch (action.type) {
+        case CategoriesPostsTypes.LOAD_POSTS_BY_CATEGORY:
+            return {
+                ...state,
+                loadingCategoriesPosts: true,
+            }
+        case CategoriesPostsTypes.LOAD_POSTS_BY_CATEGORY_SUCCESS:
+            return {
+                ...state,
+                loadingCategoriesPosts: false,
+                categoriesPosts: action.payload.categoriesPosts
+            }
+        case CategoriesPostsTypes.LOAD_POSTS_BY_CATEGORY_FAILURE:
+            return {
+                ...state,
+                loadingCategoriesPosts: false,
+                errorCategoriesPosts: true
+            }
+        default:
+            return state
+    }
+
+}
+
+export default reducer
