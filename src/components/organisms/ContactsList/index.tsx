470de3eaf14d06f51c577ca4fc79b286797b63ff
Caique <caiquetorres.bastos@hotmail.com>
2020-09-20 15:43:53 -0300

    feat(ContactsList/index.tsx & ContactsList/styles.ts): create the basic files of the ContactsList

---
+++ src/components/organisms/ContactsList/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { ContainerView } from './styles'
+
+interface ContactsListProps {
+
+}
+
+const ContactsList: React.FC<ContactsListProps> = () => {
+    return (
+        <ContainerView />
+    )
+}
+
+export default ContactsList
