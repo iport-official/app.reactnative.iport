b08e871d0e1923e15f30c1e443a44e5c022e8cd0
Michell Algarra <michell.algarra@gmail.com>
2020-09-24 22:22:31 -0300

    feat(ProfileHighlights): Created a component to group all the HighlightItem components

---
+++ src/components/molecules/ProfileHighlights/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const ProfileHighlightsContainer = styled.View`
+    flex: 1;
+    width: 100%;
+    align-items: center;
+`
+
+export const ProfilePersonalInfoContainer = styled.View`
+    margin: 20px 0;
+    width: 98%;
+`
+
+export const HighlightItemsContainer = styled.View`
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+
+    width: 98%;
+    margin-bottom: 40px;
+`
