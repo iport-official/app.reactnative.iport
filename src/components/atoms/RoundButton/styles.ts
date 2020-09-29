5e9816dc1bba2614845c58d530c9d061b99cd13f
Michell Algarra <michell.algarra@gmail.com>
2020-09-29 19:41:37 -0300

    feat(RoundButton): Added some props to make it easier to animate

--- src/components/atoms/RoundButton/styles.ts
+++ src/components/atoms/RoundButton/styles.ts
@@ -1,17 +1,15 @@
 import styled from 'styled-components/native';
+import { Animated } from 'react-native';
 
 const size = 60;
 
-export const ButtonContainer = styled.View`
-    position: absolute;
-    bottom: 10px;
-    right: 10px;
-
+export const ButtonContainer = styled(Animated.View)`
     justify-content: center;
     align-items: center;
 
     height: ${size}px;
     width: ${size}px;
+    margin-top: 10px;
 
     border-radius: ${size * 2}px;
 `
