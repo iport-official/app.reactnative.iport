0e9d8bb834df4eb38f0564ba5b2eb7edc0370221
Caique <caiquetorres.bastos@hotmail.com>
2020-09-29 19:26:45 -0300

    feat(InputField/index.tsx & InputField/styles.ts): create the base files

---
+++ src/components/atoms/InputField/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View } from 'react-native'
+
+interface InputFieldProps {
+
+}
+
+const InputField: React.FC<InputFieldProps> = () => {
+    return (
+        //#region JSX
+
+        <View />
+
+        //#endregion
+    )
+}
+
+export default InputField
