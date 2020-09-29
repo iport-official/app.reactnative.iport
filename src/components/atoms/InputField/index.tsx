c09ba23ff5642e91ecb192b9af7beaebf762f714
Caique <caiquetorres.bastos@hotmail.com>
2020-09-29 20:23:26 -0300

    feat(InputField/indext.tsx & InputField/styles.ts): start creating the styles and the main structure of this component

--- src/components/atoms/InputField/index.tsx
+++ src/components/atoms/InputField/index.tsx
@@ -1,18 +1,57 @@
-import React from 'react'
-import { View } from 'react-native'
+import React from "react";
+import { StyleProp, TextInputProps, ViewStyle } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-interface InputFieldProps {
+import {
+    ContainerView,
+    TextInputView,
+    PlaceholderView,
+    PlaceholderText,
+    MainTextInput,
+    DescriptionText,
+} from "./styles";
 
+interface InputFieldProps extends TextInputProps {
+    password: boolean;
+    description?: string;
+    information?: string
+    style?: StyleProp<ViewStyle>;
 }
 
-const InputField: React.FC<InputFieldProps> = () => {
+const InputField: React.FC<InputFieldProps> = ({
+    placeholder,
+    description,
+    information,
+    style,
+}) => {
     return (
         //#region JSX
 
-        <View />
+        <ContainerView style={style}>
+            <TextInputView>
+                <MainTextInput />
+                <PlaceholderView pointerEvents="none">
+                    <PlaceholderText>{placeholder}</PlaceholderText>
+                </PlaceholderView>
+            </TextInputView>
+
+            {information && <Ionicons
+                style={{
+                    position: "absolute",
+                    right: 20
+                }}
+                name="ios-information-circle-outline"
+                size={30}
+                color="#B09AC7"
+            />}
+
+            {description && (
+                <DescriptionText>100 - 300 palavras</DescriptionText>
+            )}
+        </ContainerView>
 
         //#endregion
-    )
-}
+    );
+};
 
-export default InputField
+export default InputField;
--- src/components/atoms/InputField/styles.ts
+++ src/components/atoms/InputField/styles.ts
@@ -1,5 +1,57 @@
+import { Animated } from 'react-native'
 import styled from 'styled-components/native'
 
-export const ContainerView = styled.View`
+import { colors } from '../../../styles'
 
+const paddingHorizontal = 20
+const height = 55
+const width = 350
+
+export const ContainerView = styled.View`
+    width: ${width}px;
+    height: ${height}px;
+    padding: 0 ${paddingHorizontal}px;
+    border-radius: 8px;
+    background: #fff;
+    flex-direction: row;
+    align-items: center;
 `
+
+export const TextInputView = styled.View`
+    flex: 1;
+    height: 100%;
+    flex-direction: row;
+    align-items: center;
+`
+
+export const PlaceholderView = styled.View`
+    position: absolute;
+`
+
+export const PlaceholderText = styled(Animated.Text)`
+    color: ${colors.livePurple};
+    opacity: 0.3;
+    font-size: 16px;
+    font-family: Roboto_700Bold;
+`
+
+export const MainTextInput = styled.TextInput`
+    flex: 1;
+    height: 100%;
+    padding: 0;
+    font-family: Roboto_400Regular;
+    border-bottom-width: 3px;
+    border-bottom-color: ${colors.livePurple};
+`
+
+export const DescriptionText = styled.Text`
+    position: absolute;
+    top: ${height + 5}px;
+    left: ${paddingHorizontal - 10}px;
+    color: ${colors.livePurple};
+    opacity: 0.3;
+    font-family: Roboto_400Regular;
+`
+
+// Roboto_400Regular
+// Roboto_700Bold
