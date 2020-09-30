d24738bdd5c42baf51deb848f0f9b69cb5ed6b0f
Caique <caiquetorres.bastos@hotmail.com>
2020-09-30 10:46:52 -0300

    feat(PostCreation/index.tsx): add the validated property in InputField components

--- src/pages/PostCreation/index.tsx
+++ src/pages/PostCreation/index.tsx
@@ -19,11 +19,13 @@ export default function PostCreationPage() {
                 <InputField
                     color={colors.livePurple}
                     multiline
+                    validated
                     description="100 - 300 pala jakds "
                     information="A descrição deve falar sobre o que é essa tal vaga que você ta anunciando"
                     placeholder="Descrição"
                 />
                 <InputField
+                    validated={false}
                     color={colors.livePurple}
                     placeholder="Nome"
                     information="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
