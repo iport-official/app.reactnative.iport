27c42b24a5fb87f09e45a14f4747bffe1240eca7
Caique <caiquetorres.bastos@hotmail.com>
2020-09-30 08:40:33 -0300

    feat(InfoBox/index.tsx & InfoBox/styles.ts): create the main files of this component

---
+++ src/components/atoms/InfoBox/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import {
+    ContainerView
+} from './styles'
+
+interface InfoBoxProps {
+
+}
+
+const InfoBox: React.FC<InfoBoxProps> = ({ }) => {
+    return (
+        //#region JSX
+
+        <ContainerView />
+
+        //#endregion
+    )
+}
+
+export default InfoBox
