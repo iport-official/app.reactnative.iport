cf7c26752eb9ba960b14bdd9e8542b7d50427b42
Caique <caiquetorres.bastos@hotmail.com>
2020-09-29 21:29:12 -0300

    feat(InputField/index.tsx & InputField/styles.ts): add some animations in text input

--- src/components/atoms/InputField/index.tsx
+++ src/components/atoms/InputField/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleProp, TextInputProps, ViewStyle } from "react-native";
+import React, { useRef, useState } from "react";
+import { Animated, StyleProp, TextInputProps, ViewStyle } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import {
@@ -7,47 +7,145 @@ import {
     TextInputView,
     PlaceholderView,
     PlaceholderText,
-    MainTextInput,
+    ContainerTextInput,
+    LineView,
     DescriptionText,
 } from "./styles";
 
 interface InputFieldProps extends TextInputProps {
-    password: boolean;
+    duration?: number;
+    password?: boolean;
     description?: string;
-    information?: string
+    information?: string;
     style?: StyleProp<ViewStyle>;
 }
 
 const InputField: React.FC<InputFieldProps> = ({
+    duration = 200,
+    password = false,
     placeholder,
     description,
     information,
     style,
 }) => {
+    const [focused, setFocused] = useState(false);
+
+    const textInputPositionAnimation = useRef(new Animated.Value(0)).current;
+    const lineWidthAnimation = useRef(new Animated.Value(0)).current;
+
+    const placeholderOpacityAnimation = useRef(new Animated.Value(0.3)).current;
+    const placeholderTopAnimation = useRef(new Animated.Value(15)).current;
+    const placehodlerFontSizeAnimation = useRef(new Animated.Value(16)).current;
+
+    function handleOnFocus() {
+        Animated.parallel([
+            Animated.timing(placehodlerFontSizeAnimation, {
+                toValue: 12,
+                duration,
+                useNativeDriver: false,
+            }),
+            Animated.timing(placeholderTopAnimation, {
+                toValue: 0,
+                duration,
+                useNativeDriver: false,
+            }),
+            Animated.timing(textInputPositionAnimation, {
+                toValue: 4,
+                duration,
+                useNativeDriver: false,
+            }),
+            Animated.timing(placeholderOpacityAnimation, {
+                toValue: 1,
+                duration,
+                useNativeDriver: false,
+            }),
+            Animated.timing(lineWidthAnimation, {
+                toValue: 1,
+                duration,
+                useNativeDriver: false,
+            }),
+        ]).start();
+    }
+
+    function handleOnBlur() {
+        Animated.parallel([
+            Animated.timing(placehodlerFontSizeAnimation, {
+                toValue: 16,
+                duration,
+                useNativeDriver: false,
+            }),
+            Animated.timing(placeholderTopAnimation, {
+                toValue: 16,
+                duration,
+                useNativeDriver: false,
+            }),
+            Animated.timing(textInputPositionAnimation, {
+                toValue: 0,
+                duration,
+                useNativeDriver: false,
+            }),
+            Animated.timing(placeholderOpacityAnimation, {
+                toValue: 0.3,
+                duration,
+                useNativeDriver: false,
+            }),
+            Animated.timing(lineWidthAnimation, {
+                toValue: 0,
+                duration,
+                useNativeDriver: false,
+            }),
+        ]).start();
+    }
+
     return (
         //#region JSX
 
         <ContainerView style={style}>
             <TextInputView>
-                <MainTextInput />
-                <PlaceholderView pointerEvents="none">
-                    <PlaceholderText>{placeholder}</PlaceholderText>
+                <ContainerTextInput
+                    onFocus={handleOnFocus}
+                    onBlur={handleOnBlur}
+                    style={{
+                        transform: [{ translateY: textInputPositionAnimation }],
+                    }}
+                />
+                <PlaceholderView
+                    pointerEvents="none"
+                    style={{ top: placeholderTopAnimation }}
+                >
+                    <PlaceholderText
+                        style={{
+                            fontSize: placehodlerFontSizeAnimation,
+                            opacity: placeholderOpacityAnimation,
+                        }}
+                    >
+                        {placeholder}
+                    </PlaceholderText>
                 </PlaceholderView>
+                <LineView
+                    pointerEvents="none"
+                    style={{
+                        width: lineWidthAnimation.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: ["0%", "100%"],
+                        }),
+                    }}
+                />
             </TextInputView>
 
-            {information && <Ionicons
-                style={{
-                    position: "absolute",
-                    right: 20
-                }}
-                name="ios-information-circle-outline"
-                size={30}
-                color="#B09AC7"
-            />}
-
-            {description && (
-                <DescriptionText>100 - 300 palavras</DescriptionText>
+            {information && (
+                <Ionicons
+                    style={{
+                        position: "absolute",
+                        right: 20,
+                    }}
+                    name="ios-information-circle-outline"
+                    size={30}
+                    color="#B09AC7"
+                />
             )}
+
+            {description && <DescriptionText>{description}</DescriptionText>}
         </ContainerView>
 
         //#endregion
--- src/components/atoms/InputField/styles.ts
+++ src/components/atoms/InputField/styles.ts
@@ -1,6 +1,8 @@
 import { Animated } from 'react-native'
 import styled from 'styled-components/native'
 
+import AnimatedTextInput from '../../animated/AnimatedTextInput'
+
 import { colors } from '../../../styles'
 
 const paddingHorizontal = 20
@@ -24,7 +26,7 @@ export const TextInputView = styled.View`
     align-items: center;
 `
 
-export const PlaceholderView = styled.View`
+export const PlaceholderView = styled(Animated.View)`
     position: absolute;
 `
 
@@ -35,11 +37,18 @@ export const PlaceholderText = styled(Animated.Text)`
     font-family: Roboto_700Bold;
 `
 
-export const MainTextInput = styled.TextInput`
+export const ContainerTextInput = styled(AnimatedTextInput)`
     flex: 1;
     height: 100%;
     padding: 0;
+    padding-right: ${paddingHorizontal + 10}px;
     font-family: Roboto_400Regular;
+`
+
+export const LineView = styled(Animated.View)`
+    position: absolute;
+    width: 100%;
+    height: 100%;
     border-bottom-width: 3px;
     border-bottom-color: ${colors.livePurple};
 `
@@ -48,8 +57,9 @@ export const DescriptionText = styled.Text`
     position: absolute;
     top: ${height + 5}px;
     left: ${paddingHorizontal - 10}px;
-    color: ${colors.livePurple};
+    right: ${paddingHorizontal - 10}px;
     opacity: 0.3;
+    color: ${colors.livePurple};
     font-family: Roboto_400Regular;
 `
 
