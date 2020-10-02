9ab72117cecf83fa19664745cb24aa96f7ff4956
Caique <caiquetorres.bastos@hotmail.com>
2020-10-01 22:03:01 -0300

    feat(navigations/MainDrawer.tsx & DrawerContent/index.tsx): add the navigation logic to start creating the post creation page

--- src/navigations/MainDrawer.tsx
+++ src/navigations/MainDrawer.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { StackScreenProps } from '@react-navigation/stack';
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { StackScreenProps } from "@react-navigation/stack";
 
-import MainPage from '../pages/Main';
-import ProfilePage from '../pages/Profile';
-import DrawerContent from '../components/molecules/DrawerContent';
-import { AppStackParamsList } from './AppStack';
+import { AppStackParamsList } from "./AppStack";
+import MainPage from "../pages/Main";
+import ProfilePage from "../pages/Profile";
+import PostCreationPage from "../pages/PostCreation";
+import DrawerContent from "../components/molecules/DrawerContent";
 
-import { colors } from '../styles';
+import { colors } from "../styles";
 
 export type DrawerParamsList = {
-    MainPage: undefined
-    ProfilePage: undefined
-}
+    MainPage: undefined;
+    ProfilePage: undefined;
+    PostCreationPage: undefined;
+};
 
-type DefaultLoginPageProps = StackScreenProps<
-    AppStackParamsList,
-    "Drawer"
->
+type DefaultLoginPageProps = StackScreenProps<AppStackParamsList, "Drawer">;
 
-const { Navigator, Screen } = createDrawerNavigator<DrawerParamsList>()
+const { Navigator, Screen } = createDrawerNavigator<DrawerParamsList>();
 
 export default function Drawer({ navigation }: DefaultLoginPageProps) {
     return (
@@ -27,15 +26,17 @@ export default function Drawer({ navigation }: DefaultLoginPageProps) {
 
         <Navigator
             initialRouteName="MainPage"
-            drawerContent={props => <DrawerContent {...props} />}
+            drawerContent={(props) => <DrawerContent {...props} />}
             drawerStyle={{
                 width: "80%",
-                backgroundColor: colors.vividPurple + 'dd'
-            }}>
+                backgroundColor: colors.vividPurple + "dd",
+            }}
+        >
             <Screen name="MainPage" component={MainPage} />
+            <Screen name="PostCreationPage" component={PostCreationPage} />
             <Screen name="ProfilePage" component={ProfilePage} />
         </Navigator>
 
         //#endregion
-    )
+    );
 }
