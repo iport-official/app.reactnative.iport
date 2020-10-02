5b1ed9d434f8fdf64f9bf5349be69ad5a8a28176
Caique <caiquetorres.bastos@hotmail.com>
2020-10-01 22:03:43 -0300

    feat(PostCreation/index.tsx & PostCreation/styles.ts): create the page header

--- src/pages/PostCreation/index.tsx
+++ src/pages/PostCreation/index.tsx
@@ -1,38 +1,70 @@
-import React from 'react'
+import React from "react";
+import { AntDesign } from "@expo/vector-icons";
 
 import {
     ContainerSafeAreaView,
-    TextInputsView
-} from './styles'
+    HeaderView,
+    TitleText,
+    ContainerScrollView,
+} from "./styles";
 
-import InputField from '../../components/molecules/InputField'
+import MainHeader from "../../components/molecules/MainHeader";
+import { DrawerScreenProps } from "@react-navigation/drawer";
+import { DrawerParamsList } from "../../navigations/MainDrawer";
+import { StatusBar } from "expo-status-bar";
+import { colors } from "../../styles";
 
-import { colors } from '../../styles'
+type DefaultPostCreationPageProps = DrawerScreenProps<
+    DrawerParamsList,
+    "PostCreationPage"
+>;
 
-export default function PostCreationPage() {
+export default function PostCreationPage({
+    navigation,
+}: DefaultPostCreationPageProps) {
     return (
         //#reigon JSx
 
         <ContainerSafeAreaView>
-            <TextInputsView>
-                <InputField
-                    color={colors.livePurple}
-                    multiline
-                    validated={false}
-                    description="100 - 300 palavras "
-                    information="A descrição deve falar sobre o que é essa tal vaga que você ta anunciando"
-                    errorMessage="Quantidade de caracteres excedida"
-                    placeholder="Descrição"
+            <StatusBar style="light" backgroundColor="black" />
+            <MainHeader onPress={() => {}} />
+            <HeaderView>
+                <AntDesign
+                    style={{
+                        position: "absolute",
+                        left: 15,
+                    }}
+                    name="arrowleft"
+                    size={24}
+                    color="#303030"
                 />
-                <InputField
-                    validated
-                    color={colors.livePurple}
-                    placeholder="Nome"
-                    information="Lorem ipsum"
-                />
-            </TextInputsView>
+                <TitleText>Crie seu Post</TitleText>
+            </HeaderView>
+            <ContainerScrollView></ContainerScrollView>
         </ContainerSafeAreaView>
 
         //#endregion
-    )
+    );
 }
+
+/*
+
+<TextInputsView>
+    <InputField
+        color={colors.livePurple}
+        multiline
+        validated={false}
+        description="100 - 300 palavras "
+        information="A descrição deve falar sobre o que é essa tal vaga que você ta anunciando"
+        errorMessage="Quantidade de caracteres excedida"
+        placeholder="Descrição"
+    />
+    <InputField
+        validated
+        color={colors.livePurple}
+        placeholder="Nome"
+        information="Lorem ipsum"
+    />
+</TextInputsView>
+
+*/
--- src/pages/PostCreation/styles.ts
+++ src/pages/PostCreation/styles.ts
@@ -1,12 +1,25 @@
+import { Roboto_400Regular } from '@expo-google-fonts/roboto'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
 
-export const ContainerSafeAreaView = styled.SafeAreaView`
+export const ContainerSafeAreaView = styled(SafeAreaView)`
     flex: 1;
+`
+
+export const HeaderView = styled.View`
+    height: 60px;
     justify-content: center;
     align-items: center;
 `
 
-export const TextInputsView = styled.View`
-    justify-content: space-between;
-    height: 300px;
+export const TitleText = styled.Text`
+    color: #303030;
+    font-size: 17px;
+    font-family: Roboto_700Bold;
 `
+
+export const ContainerScrollView = styled.ScrollView`
+    flex: 1;
+`
+// Roboto_400Regular
+// Roboto_700Bold
