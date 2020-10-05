ccb37a615962debae963d4b98fe190a8a3b04425
Michell Algarra <michell.algarra@gmail.com>
2020-10-04 21:12:37 -0300

    feat(profile-page): Changed action button animation values to make it faster and added a new button to show the user additional contacts

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Animated, View } from 'react-native';
+import { Animated, Modal, Text, View } from 'react-native';
 import { DrawerScreenProps } from '@react-navigation/drawer';
 import { setStatusBarStyle } from 'expo-status-bar';
 import { AntDesign } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import {
+    ContactItem,
+    ContactTitle,
     ContainerSafeAreaView,
     ContainerKeyboardAvoidView,
-    ContentView
+    ContentView,
+    ModalContainer,
+    ModalContent
 } from './styles';
 
 import { DrawerParamsList } from '../../navigations/MainDrawer';
@@ -35,13 +39,18 @@ export default function ProfilePage({ navigation }: DefaultMainPageProps) {
 
     useEffect(() => { setStatusBarStyle("light") }, []);
 
-
     const [isActive, setIsActive] = useState(false);
     const [first, setFirst] = useState(0);
     const [liked, setLiked] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
 
-    const animatedPopA = useRef(new Animated.Value(70)).current;
-    const animatedPopB = useRef(new Animated.Value(140)).current;
+    const buttonPosA: number = 70;
+    const buttonPosB: number = 140;
+    const buttonPosC: number = 210;
+
+    const animatedPopA = useRef(new Animated.Value(buttonPosA)).current;
+    const animatedPopB = useRef(new Animated.Value(buttonPosB)).current;
+    const animatedPopC = useRef(new Animated.Value(buttonPosC)).current;
     const animatedSpin = useRef(new Animated.Value(0)).current;
 
     const moreButtonAnimated = () => {
@@ -60,6 +69,10 @@ export default function ProfilePage({ navigation }: DefaultMainPageProps) {
         return <MaterialCommunityIcons name="briefcase-check" size={30} color="white" />
     }
 
+    const contactsButton = () => {
+        return <MaterialCommunityIcons name="phone" size={30} color="white" />
+    }
+
     const toggleActionButton = () => {
         setIsActive(!isActive);
         setFirst(1);
@@ -71,7 +84,6 @@ export default function ProfilePage({ navigation }: DefaultMainPageProps) {
     }
 
     const handleLikePress = () => {
-        toggleActionButton();
         setLiked(!liked);
     }
 
@@ -80,21 +92,31 @@ export default function ProfilePage({ navigation }: DefaultMainPageProps) {
         alert('Applied');
     }
 
+    const handleContactsPress = () => {
+        toggleActionButton();
+        setModalVisible(true);
+    }
+
     const animatePopIn = () => {
         Animated.parallel([
             Animated.timing(animatedPopA, {
                 toValue: 0,
-                duration: 800,
+                duration: 400,
                 useNativeDriver: true
             }),
             Animated.timing(animatedPopB, {
                 toValue: 0,
-                duration: 800,
+                duration: 400,
+                useNativeDriver: true
+            }),
+            Animated.timing(animatedPopC, {
+                toValue: 0,
+                duration: 400,
                 useNativeDriver: true
             }),
             Animated.timing(animatedSpin, {
                 toValue: 1,
-                duration: 800,
+                duration: 400,
                 useNativeDriver: true
             })
         ]).start();
@@ -103,18 +125,23 @@ export default function ProfilePage({ navigation }: DefaultMainPageProps) {
     const animatePopOut = () => {
         Animated.parallel([
             Animated.timing(animatedPopA, {
-                toValue: 70,
-                duration: 800,
+                toValue: buttonPosA,
+                duration: 400,
                 useNativeDriver: true
             }),
             Animated.timing(animatedPopB, {
-                toValue: 140,
-                duration: 800,
+                toValue: buttonPosB,
+                duration: 400,
+                useNativeDriver: true
+            }),
+            Animated.timing(animatedPopC, {
+                toValue: buttonPosC,
+                duration: 400,
                 useNativeDriver: true
             }),
             Animated.timing(animatedSpin, {
                 toValue: 0,
-                duration: 800,
+                duration: 400,
                 useNativeDriver: true
             })
         ]).start();
@@ -125,11 +152,38 @@ export default function ProfilePage({ navigation }: DefaultMainPageProps) {
         outputRange: ['180deg', '0deg']
     });
 
+    // TODO - remove template arrays
+    const phones: String[] = ["988776655", "988776655", "988776655"];
+    const emails: String[] = ["michell@gmail.com", "michell.algarra@gmail.com", "nest.js@gmail.com"];
+
     return (
         //#region JSX
 
         <ContainerSafeAreaView>
             <ContainerKeyboardAvoidView>
+                <Modal
+                    animationType='fade'
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => setModalVisible(false)} >
+                    <ModalContainer
+                        onPress={() => setModalVisible(false)}
+                        activeOpacity={1} />
+                    <ModalContent>
+                        { emails.length > 0 ? <ContactTitle>E-mails</ContactTitle> : <View /> }
+                        { emails.map(e => {
+                            return <ContactItem>{ e }</ContactItem>
+                        }) }
+                        { phones.length > 0 ? <ContactTitle style={{ marginTop: 20 }}>Telefones</ContactTitle> : <View /> }
+                        { phones.map(p => {
+                            return <ContactItem>{ p }</ContactItem>
+                        }) }
+                        { emails.length === 0 && phones.length === 0
+                            ? <ContactTitle style={{ textAlign: 'center', marginTop: 20 }}
+                                >Este usuário não adicionou nenhum contato adicional</ContactTitle>
+                            : <View /> }
+                    </ModalContent>
+                </Modal>
                 <MainHeader onPress={() => { navigation.openDrawer() }} />
                 <ContentView
                     showsVerticalScrollIndicator={false}
@@ -150,17 +204,23 @@ export default function ProfilePage({ navigation }: DefaultMainPageProps) {
                         icon={moreButtonAnimated}>
                         { first == 0 ? <View /> : <View>
                             <RoundButton
-                                transform={first == 0 ? 140 : animatedPopB }
+                                transform={first == 0 ? buttonPosC : animatedPopC }
                                 spin={first == 0 ? '180deg' : spin}
                                 bgColor='#46266c'
                                 icon={checkButton}
                                 onPress={() => handleApplyPress()}  />
                             <RoundButton
-                                transform={first == 0 ? 70 : animatedPopA}
+                                transform={first == 0 ? buttonPosB : animatedPopB}
                                 spin={first == 0 ? '180deg' : spin}
                                 bgColor='#cc4195'
                                 icon={liked ? likeButtonFilled : likeButton}
                                 onPress={() => handleLikePress()} />
+                            <RoundButton
+                                transform={first == 0 ? buttonPosA : animatedPopA}
+                                spin={first == 0 ? '180deg' : spin}
+                                bgColor='#850085'
+                                icon={contactsButton}
+                                onPress={() => handleContactsPress()} />
                             </View>
                         }
                     </ActionButton>
