b5fef4b01c3e213652414e650e129a3f97673cfc
Michell Algarra <michell.algarra@gmail.com>
2020-10-10 19:10:04 -0300

    style: Removed commented lines

--- src/contexts/actionButton.tsx
+++ src/contexts/actionButton.tsx
@@ -1,23 +1,9 @@
-import React, { createContext, useState } from 'react';
+import { createContext } from 'react';
 
 interface ActionButtonContextData {
     isActive: boolean;
 }
 
 const ActionButtonContext = createContext<ActionButtonContextData>({} as ActionButtonContextData);
-
-// export const ActionButtonProvider: React.FC = ({ children }) => {
-//     const [active, setActive] = useState(false);
-
-//     async function toggleButton() {
-//         await setActive(!active);
-//     }
-
-//     return (
-//         <ActionButtonContext.Provider value={{ isActive: active, toggleButton }}>
-//             { children }
-//         </ActionButtonContext.Provider>
-//     )
-// }
 
 export default ActionButtonContext;
