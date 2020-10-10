7a593c661ed8214f0f7e280d241af247e92833c1
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-10 12:26:40 -0300

    feat(utils/rules.ts): add some methods in rules to make easier validate data in the project

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,4 +1,27 @@
 const emailRegex = /^(([^<>()\\.,;:\s@"]+(\.[^<>()\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})/;
 
-export const rules = { emailRegex, passwordRegex }
+export function validateEmail(email: string): boolean {
+    return emailRegex.test(email);
+}
+
+export function validatePassword(password: string): boolean {
+    return passwordRegex.test(password);
+}
+
+export function validateSentenceLength(
+    sentence: string,
+    maxLength: number
+): boolean {
+    return sentence.length <= maxLength;
+}
+
+export function validateSentenceIsEmpty(sentence: string) {
+    return (!sentence || 0 === sentence.length);
+}
+
+export const rules = {
+    validateEmail,
+    validatePassword,
+    validateSentenceLength,
+};
