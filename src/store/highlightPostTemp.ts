7e055e1eea530cbc9dba2f3fe2b8ef994f0bfe72
Michell Algarra <michell.algarra@gmail.com>
2020-10-14 00:26:33 -0300

    feat(store/highlightPostTemp.ts): Created a temporary file to keep the UpdatePostPayload

---
+++ src/store/highlightPostTemp.ts
@@ -0,0 +1,14 @@
+export interface UpdatePostPayload {
+    image?: string
+    title?: string
+    description?: string
+    category?: string
+    recomendation?: number
+    contact?: string
+    salary?: number
+    role?: string
+    local?: string
+    requirements?: string
+    experienceLevel?: string
+    jobDescription?: string
+}
